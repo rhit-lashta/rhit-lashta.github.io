86237976296727d42ae5a34157e4a6735d7c0fff
Tyson Lash <lashta@rose-hulman.edu>
2025-09-24 12:18:15 -0400

    Footer added

--- scripts/loadHeaders.js
+++ scripts/loadHeaders.js
@@ -5,3 +5,9 @@ fetch("navTop.html")
     .then(data => {
     document.getElementById("navbar").innerHTML = data;
 });
+
+fetch("footer.html")
+    .then(response => response.text())
+    .then(data => {
+    document.getElementById("footer").innerHTML = data;
+});
